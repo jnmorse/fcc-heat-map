61868834d6ccdb7764ba8b3b15569ae6e6a7b446
Joseph Morse <tamed.lionheart@gmail.com>
2016-06-11 19:18:13 -0400

    Added Viewport and Charset to HTML

--- server/index.js
+++ server/index.js
@@ -27,9 +27,12 @@ const html = (html, state) => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <title>Heat Map</title>
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
